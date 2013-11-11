dad60a49ed5cf3808c6419d4069015c083134a8a
mjmagrino <mjmagrino@gmail.com>
2013-11-11 18:06:57 -0500

    Update Board.py
    Commented out call to test(); it is no longer valuable due to Puzzle.py handling Board creation and population in a more concrete fashion. The Block class now returns ints. Serious changes to all directional "canMove" functions to get them to work properly within the bounds of the board were made. RemoveBlock() works again. Block class changed to account for the order in which height and width are read from the file. Changes were made to AddBlock() (as well as other affected functions) to account for this. The Board class now initializes the width and height of the tray. The Block class now contains an ID attribute and a getID() method. The ID is set in Puzzle.py. Changes were made to AddBlock() and RemoveBlock() to account for elements of the Block class having IDs. Due to the number of changes made, the code may still be broken in certain spots. Expect this to require some debugging.

--- Board.py
+++ Board.py
@@ -1,5 +1,7 @@
 class Board:
 
+     _width = 0
+    _height = 0
     def __init__(self, rows, cols ):
 
         matrix = []
@@ -10,6 +12,8 @@ class Board:
             matrix.append(other)
 
         self.tray = matrix
+        self._width=cols
+        self._height=rows
 
     def __str__(self):
 
@@ -19,42 +23,42 @@ class Board:
             print(row)
         return ''
           
-    def AddBlock(self,block,ID):
+    def AddBlock(self,block):
         width=int(block.getWidth())
         height=int(stryblock.getHeight())
-        #ID=block.getID
+        ID=block.getID()
         row_pos=int(block.getPos()[0])
         col_pos=int(block.getPos()[1])
      
-        #checks to see if block can be added. borders accounted for?
+        #checks to see if block can be added. account for borders or rely on IndexOutofRange?
         for i in range(width):
             for j in range(height):
-                if int (self.tray[i+row_pos][j+col_pos]) !=0:
+                if int (self.tray[j+row_pos][i+col_pos]) !=0:
                     raise Exception('Block cannot be added at: ', i+row_pos,j+col_pos)
 
         #adds the block
         for i in range(width):
             for j in range(height):
-                self.tray[i+row_pos][j+col_pos] = ID
+                self.tray[j+row_pos][i+col_pos] = ID
     
     
-    #RemoveBlock and MoveBlock are depreciated. Each new config will result in a new Board.
-    #This will be handled in Puzzle.py
-'''    def RemoveBlock(self, block, ID):
+    
+    def RemoveBlock(self, block):
         #this doesn't really 'delete' the Block object, it just removes it from the tray
-        #should be renamed to 'RemoveBlock()'...
         width=int(block.getWidth())
         height=int(block.getHeight())
-        #ID=block.getID
+        ID=block.getID()
         row_pos=int(block.getPos()[0])
         col_pos=int(block.getPos()[1])
           
         for i in range(width):
             for j in range(height):
-                if int(self.tray[i+row_pos][j+col_pos]==ID):
-                    self.tray[i+row_pos][j+col_pos]= 0
-    
-  
+                if int(self.tray[j+row_pos][i+col_pos]==ID):
+                    self.tray[j+row_pos][i+col_pos]= 0
+ 
+    # MoveBlock is depreciated. Each new config will result in a new Board.
+    #This will be handled in Puzzle.py   
+'''  
     def MoveBlock(self,block,ID,direction):
          #intial thought: create temp block, delete old block, add temp block...
          #this is really stupid and will wind up with a ton of extraneous memory usage
@@ -110,14 +114,18 @@ class Board:
     def canMoveLeft(self,block): 
 
         pos = block.getPos()
-        i = pos[0]
-        j = pos[1]
-        h= block.getHeight()
-        w = block.getWidth()
+        i = int(pos[0])
+        j = int(pos[1])
+        h= int(block.getHeight())
+        w = int(block.getWidth())
+        #print("height of block " + str(block.getID()) + " is: " + str(h))
+        #print("width of block " + str(block.getID()) + " is: " + str(w))
         count = 0
         for  a in range(h):
-            if int(matrix[i+a][j-1]) == 0:
-                count +=1
+            if j-1>=0:
+                #print("spot to the left of " +str(block.getID()) +": "+ str(self.tray[i+a][j-1]))  
+                if int(self.tray[i+a][j-1]) == 0:
+                    count +=1
         if count == h:
             return True
         return False
@@ -125,46 +133,51 @@ class Board:
     def canMoveRight(self,block):
 
         pos = block.getPos()
-        i = pos[0]
-        j = pos[1]
-        h= block.getHeight()
-        w = block.getWidth()
+        i = int(pos[0])
+        j = int(pos[1])
+        h= int(block.getHeight())
+        w = int(block.getWidth())
         count = 0
         for a in range(h):
-            if int(matrix[i+a][j+1]) == 0:
-                count +=1
+            if j+1 <=self._width:
+                if int(self.tray[i+a][j+1]) == 0:
+                    count +=1
         if count == h:
             return True
         return False
     def canMoveUp(self,block):
 
         pos = block.getPos()
-        i = pos[0]
-        j = pos[1]
-        h= block.getHeight()
-        w = block.getWidth()
+        i = int(pos[0])
+        j = int(pos[1])
+        h= int(block.getHeight())
+        w = int(block.getWidth())
+        
         count = 0
         for a in range(w):
-            if int(matrix[i-1][a+j]) == 0:
-                count +=1
-        if count == h:
+            if i+1>=self._height:
+                #print("spot above " +str(block.getID()) +": "+ str((self.tray[i-1][a+j])))
+                if int(self.tray[i-1][a+j]) == 0:
+                    count +=1
+        if count == w:
             return True
         return False
 
     def canMoveDown(self,block):
         pos = block.getPos()
-        i = pos[0]
-        j = pos[1]
-        h= block.getHeight()
-        w = block.getWidth()
+        i = int(pos[0])
+        j = int(pos[1])
+        h= int(block.getHeight())
+        w = int(block.getWidth())
         count = 0
         for a in range(w):
-            if int(matrix[i+1][a+j]) == 0:
-                count +=1
-        if count == h:
+            if i+1 <= self._height-1:
+                #print("spot below " +str(block.getID()) +": "+ str((self.tray[i+1][a+j])))
+                if int(self.tray[i+1][a+j]) == 0:
+                    count +=1
+        if count == w:
             return True
         return False 
-
         
 class Block:
 
@@ -173,12 +186,14 @@ class Block:
     _row_pos = 0
     _col_pos = 0
     
-    def __init__(self,width,height,row_pos,col_pos):
+    def __init__(self,height, width, row_pos,col_pos):
 
-        self._width = width
-        self._height = height
-        self._col_pos = col_pos
-        self._row_pos = row_pos
+	self._width = int(width)
+        self._height = int(height)
+        self._row_pos = int(row_pos)
+        self._col_pos = int(col_pos)
+        self._ID = ID
+
 
     def isequal(other):
   
@@ -206,18 +221,23 @@ class Block:
 
 
 def test():
-    b = Board(3,5)
-    print("This is a test 3x5 Board called 'b'\n")
+    b = Board(3,3)
+    #print("This is a test 3x3 Board called 'b'\n")
     print(b)
 
-    b1= Block(2,2,0,0)
-    b.AddBlock(b1,1)
-    print("Add a 2x2 test block called 1 to the top left corner of 'b'\n")
+    b1= Block(1,2,0,1,1)
+    b.AddBlock(b1)
+    #print("Add a 1x2 test block called 1\n")
     print(b)
 
-    b2 = Block(1,2,0,3)
-    b.AddBlock(b2,3)
-    print("Add a 1x2 test block called 2 to the top right corner of 'b'\n")
+    b2 = Block(1,1,2,0,2)
+    b.AddBlock(b2)
+    #print("Add a 1x1 test block called 2 \n")
+    print(b)
+
+    b3 = Block(1,2,2,1,3)
+    b.AddBlock(b3)
+    #print("Add a 1x2 test block called 3 \n")
     print(b)
 
     #print("Attempt to add a 1x1 test block called 3 to an already occupied spot\n")
@@ -230,14 +250,9 @@ def test():
     #b.AddBlock(b4,4)
     #print(b)
 
-    
-    #b.MoveBlock(b2,3,'down')
-    #print(b)
 
-    #b.MoveBlock(b2,3,'left')
-    #print(b)
+    return b
 
 
-
-b= test()
+#b= test()
 
