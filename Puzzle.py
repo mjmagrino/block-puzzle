150efc0da6829c9796f9c0858665cf1084512af2
matt <mjmagrino@gmail.com>
2013-11-17 16:15:09 -0500

    added SolutionNotFound() function

--- Puzzle.py
+++ Puzzle.py
@@ -5,6 +5,22 @@ from Block import *
 hash_table = {}
 
 
+goals = []
+
+def SolutionNotFound(boardlist):
+
+    count = 0 
+    for board in boardlist:
+        for block in board.getBlockList():
+            for GoalBlock in goals:
+                if block.isequal(GoalBlock):
+                    count += 1
+    if count != len(goals):
+        return True
+    else:
+        return False
+
+
 def HashConfig(blocks):
     ints =[]
     key = ""
@@ -67,41 +83,51 @@ def main(argv):
         b = Block(tmp[0],tmp[1],tmp[2],tmp[3],i+1)            
         blocks.append(b)
 
-    print("\n~~~~~~first MakeBoard attempt~~~~~\n")
+    for i in range(len(goallines)):
+        tmp=[]
+        for j in range(len(goallines[i])):
+            if (goallines[i][j] != ' '):
+                tmp.append(goallines[i][j])
+        b = Block(tmp[0],tmp[1],tmp[2],tmp[3],i+1)            
+        goals.append(b)
+
+    #print("\n~~~~~~first MakeBoard attempt~~~~~\n")
     board = MakeBoard(board_height, board_width, blocks)
     boardlist.append(board)    
 
         #for board in boardlist:
             #print(board)
             #print(board.PossibleMoves())
-    print("~~~~intial blocks from file~~~:\n")
-    print(blocks)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")         
+    #print("~~~~intial blocks from file~~~:\n")
+    #print(blocks)
+    #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")         
     print("~~~~intial boardlist~~~~~~~~~~:\n")
     print(boardlist)
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")  
 
-    newlist =NewConfigs(boardlist)
+    x = 0
+    while(SolutionNotFound(boardlist):
+        boardlist = NewConfigs(boardlist)
+        print(x)
+        x +=1 
+    #boardlist.append(NewConfigs(boardlist))
+    #newlist =NewConfigs(boardlist)
 
 
     #print("~~~~intial boardlist after NewConfigs~~~~~~~~~~:\n")
     #print(boardlist)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")  
+    #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")  
 
     print("~~~~new boardlist~~~~~~~~~~~~~:\n")
-    print(newlist)
+    print(len(boardlist))
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n") 
     #for board in newlist:
         #print(board)
         #print(board.PossibleMoves())
 
 
-    newnewlist = NewConfigs(newlist)
-    
 
-    print("~~~~new new boardlist~~~~~~~~~~~~~:\n")
-    print(newnewlist)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n") 
+    
 
 
 
@@ -110,62 +136,62 @@ def MakeBoard(height,width,blocks):
     for x in range(len(blocks)):
         board.AddBlock(blocks[x])
     #return board, board.PossibleMoves(blocks), blocks
-    print("~~~~~~~~~~~~~~~~~~~ MakeBoard() board: " + "\n" + str(board))
+    #print("~~~~~~~~~~~~~~~~~~~ MakeBoard() board: " + "\n" + str(board))
     return board
     
 def NewConfigs(boardlist):
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NewConfigs() starts~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    #print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NewConfigs() starts~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     ret_list =[]
     
     for board in boardlist:
-        print("~~~~~~~~~~~~~~~~~~~~~1st for loop NewConfigs() board: " + "\n" + str(board))
+        #print("~~~~~~~~~~~~~~~~~~~~~1st for loop NewConfigs() board: " + "\n" + str(board))
         moves = board.PossibleMoves()
-        print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
-        print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() moves: " + str(moves))
+        #print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
+        #print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() moves: " + str(moves))
         
         #this for loop is what is currently causing the crash
         for ID in moves:
-            print("~~~~~~~~~~~~~~~~~~~~~ 2nd for loop NewConfigs() board: " + "\n" + str(board))
-            print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
-            print("~~~~~~~~~~~~~~~~NewConfigs() ID: " +str(ID) + "\n")
-            print("~~~~~~~~~~~~~~~~NewConfigs() moves[ID]: " +str(moves[ID]) + "\n")
+            #print("~~~~~~~~~~~~~~~~~~~~~ 2nd for loop NewConfigs() board: " + "\n" + str(board))
+            #print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
+            #print("~~~~~~~~~~~~~~~~NewConfigs() ID: " +str(ID) + "\n")
+            #print("~~~~~~~~~~~~~~~~NewConfigs() moves[ID]: " +str(moves[ID]) + "\n")
             for move in moves[ID]:
-                print("~~~~~~~~~~~~~~~~~~~~~3rd for loop NewConfigs() board: " + "\n" + str(board))
-                print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
-                print("~~~~~~~~~~~~~~~~NewConfigs() move: " +str(move) + "\n")
+                #print("~~~~~~~~~~~~~~~~~~~~~3rd for loop NewConfigs() board: " + "\n" + str(board))
+                #print("~~~~~~~~~~~~~~~~~~~~~NewConfigs() board.getBlockList(): " + str(board.getBlockList()))
+                #print("~~~~~~~~~~~~~~~~NewConfigs() move: " +str(move) + "\n")
                 temp_board = MakeNewBoard(board,ID,move)
                 ret_list.append(temp_board)
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NewConfigs() finishes!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    #print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NewConfigs() finishes!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     return ret_list
 
 
 def MakeNewBoard(board,ID,move):
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MakeNewBoard() starts~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    #print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MakeNewBoard() starts~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     #somewhere in the below code the BlockList of the board passed to this function is being directly added to
     #this should not happen
     blocks = board.getBlockList()
     
     newblocklist=[]
-    print("~~~~~~~~~~~~~~~~~ start of MakeNewBoard() blocks: " + str(blocks))
-    print("~~~~~~~~~~~~~~~~~ start of MakeNewBoard() board.getBlockList(): " + str(board.getBlockList()))
-    print("~~~~~~~~~~~~~~~~~MakeNewBoard() ID: "+ str(ID) + "\n")
+    #print("~~~~~~~~~~~~~~~~~ start of MakeNewBoard() blocks: " + str(blocks))
+    #print("~~~~~~~~~~~~~~~~~ start of MakeNewBoard() board.getBlockList(): " + str(board.getBlockList()))
+    #print("~~~~~~~~~~~~~~~~~MakeNewBoard() ID: "+ str(ID) + "\n")
 
     for block in blocks:
-        print("~~~~~~~~~~~~~~~~~~MakeNewBoard() block.getID() consistency check: " + "\n" +\
-        "block: " + str(block) + "\n" +\
-        "ID: " +str(ID) + "\n" +\
-        "block.getID(): " +str(block.getID()))
+        #print("~~~~~~~~~~~~~~~~~~MakeNewBoard() block.getID() consistency check: " + "\n" +\
+        #"block: " + str(block) + "\n" +\
+        #"ID: " +str(ID) + "\n" +\
+        #"block.getID(): " +str(block.getID()))
         
         if str(block.getID())==str(ID):
-            print("~~~~~hello from the OldBlock check! Success!~~~~~~\n\n") 
+            #print("~~~~~hello from the OldBlock check! Success!~~~~~~\n\n") 
             OldBlock = block
 
         elif str(block.getID())!=str(ID):
-            print("~~~~~ entered elif block.getID()!=ID~~~~~~\n\n") 
+            #print("~~~~~ entered elif block.getID()!=ID~~~~~~\n\n") 
             newblocklist.append(block)
         else:
             raise Exception("welp, this isn't working...\n")
-        print("~~~~~~current state of newblocklist: " + str(newblocklist) + "\n")
+        #print("~~~~~~current state of newblocklist: " + str(newblocklist) + "\n")
         
     if move == "Left":
         NewRowPos =OldBlock.getPos()[0]
@@ -182,47 +208,45 @@ def MakeNewBoard(board,ID,move):
     else:
         raise Exception("MakeNewBoard failed!")
 
-    print("~~~~~~~~~~~~~~OldBlock properties~~~~~~~~~~~~~~~~~~ \n" +\
-    "OldBlock.getHeight(): " + str(OldBlock.getHeight()) +"\n" +\
-    "OldBlock.getWidth(): " + str(OldBlock.getWidth()) +"\n" +\
-    "OldBlock.getPos()[0]: " + str(OldBlock.getPos()[0]) +"\n" +\
-    "OldBlock.getPos()[1]: " + str(OldBlock.getPos()[1]) +"\n" +\
-    "OldBlock.getID(): " + str(OldBlock.getID()))
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    #print("~~~~~~~~~~~~~~OldBlock properties~~~~~~~~~~~~~~~~~~ \n" +\
+    #"OldBlock.getHeight(): " + str(OldBlock.getHeight()) +"\n" +\
+    #"OldBlock.getWidth(): " + str(OldBlock.getWidth()) +"\n" +\
+    #"OldBlock.getPos()[0]: " + str(OldBlock.getPos()[0]) +"\n" +\
+    #"OldBlock.getPos()[1]: " + str(OldBlock.getPos()[1]) +"\n" +\
+    #"OldBlock.getID(): " + str(OldBlock.getID()))
+    #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
 
 
     NewBlock = Block(OldBlock.getHeight(), OldBlock.getWidth(), NewRowPos,NewColPos,OldBlock.getID())
-    #NewBlock = OldBlock
-
-    print("~~~~~~~~~~~~~~NewBlock properties~~~~~~~~~~~~~~~~~~ \n" +\
-    "NewBlock.getHeight(): " + str(NewBlock.getHeight()) +"\n" +\
-    "NewBlock.getWidth(): " + str(NewBlock.getWidth()) +"\n" +\
-    "NewBlock.getPos()[0]: " + str(NewBlock.getPos()[0]) +"\n" +\
-    "NewBlock.getPos()[1]: " + str(NewBlock.getPos()[1]) +"\n" +\
-    "NewBlock.getID(): " + str(NewBlock.getID()))
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
 
 
-    #print("~~~~~~~~~~~~~MakeNewBoard() NewBlock: " + str(NewBlock))
+    #print("~~~~~~~~~~~~~~NewBlock properties~~~~~~~~~~~~~~~~~~ \n" +\
+    #"NewBlock.getHeight(): " + str(NewBlock.getHeight()) +"\n" +\
+    #"NewBlock.getWidth(): " + str(NewBlock.getWidth()) +"\n" +\
+    #"NewBlock.getPos()[0]: " + str(NewBlock.getPos()[0]) +"\n" +\
+    #"NewBlock.getPos()[1]: " + str(NewBlock.getPos()[1]) +"\n" +\
+    #"NewBlock.getID(): " + str(NewBlock.getID()))
+    #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
     newblocklist.append(NewBlock)
 
-    print("~~~~~~~~~~~~~MakeNewBoard() newblocklist: \n")
-    print(str(newblocklist) + "\n")  
+    #print("~~~~~~~~~~~~~MakeNewBoard() newblocklist: \n")
+    #print(str(newblocklist) + "\n")  
 
     #call initial MakeBoard function...assuming this is where the first board's blocklist is double added to
     height = board._height
     width = board._width
 
-    print("~~~~~~~~~~~~~~~~~  MakeNewBoard() before creation of NewBoard board.getBlockList(): " + str(board.getBlockList()))
+    #print("~~~~~~~~~~~~~~~~~  MakeNewBoard() before creation of NewBoard board.getBlockList(): " + str(board.getBlockList()))
     #this works in creating a new Board
     NewBoard = MakeBoard(height,width,newblocklist)
 
-    #as these print statements show, something unexpected is happening where the original BlockList is being modified
-    print("~~~~~~~~~~~~~~~~~ end of MakeNewBoard() blocks: " + str(blocks))
-    print("~~~~~~~~~~~~~~~~~ end of MakeNewBoard() board.getBlockList(): " + str(board.getBlockList()))
+    
+    #print("~~~~~~~~~~~~~~~~~ end of MakeNewBoard() blocks: " + str(blocks))
+    #print("~~~~~~~~~~~~~~~~~ end of MakeNewBoard() board.getBlockList(): " + str(board.getBlockList()))
 
     
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MakeNewBoard() finishes!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    #print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MakeNewBoard() finishes!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     return NewBoard
     
 #assumes single block goal
